e45cdbd4034bc30c47a7923f6852f42f4823178f
Raghu Betina <raghu@firstdraft.com>
2020-08-06 19:00:52 +0000

    Whitelist columns in Vote for ActiveAdmin

--- app/admin/votes.rb
+++ app/admin/votes.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Vote do
 
+ permit_params :photo_id, :blah
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
