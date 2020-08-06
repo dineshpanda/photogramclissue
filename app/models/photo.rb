aadf7a2a35c2a5e21051fad7b9274e088fcf9294
Raghu Betina <raghu@firstdraft.com>
2020-08-06 19:00:37 +0000

    Indirect association between users and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -10,6 +10,10 @@ class Photo < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :commenters,
+             :through => :comments,
+             :source => :commenter
+
   has_many   :followers,
              :through => :owner,
              :source => :followers
